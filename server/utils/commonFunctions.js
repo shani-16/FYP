c5f4d1f882b3ff64750555247460484410a0e37b
Camim Khan <camim.xafar500@gmail.com>
2022-06-23 15:32:47 +0500

    integrated signUp API on frontEnd
    integrated signUp API on frontEnd .
    added code in commonfunction file

--- server/utils/commonFunctions.js
+++ server/utils/commonFunctions.js
@@ -1,8 +1,37 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+
 const genSecretPassword = async (password) => {
   const salt = await bcrypt.genSalt(10);
   return await bcrypt.hash(password, salt);
 };
+
+const HTTP_STATUS = {
+  OK: 200,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+};
+
+const successResponse = (res, statusCode, successMessage = "", token = "") =>
+  res.status(statusCode).send({
+    success: true,
+    status: "succesfull",
+    message: successMessage,
+    token: token ? token : null,
+  });
+
+const failedResponse = (res, statusCode, failedMessage = "") =>
+  res
+    .status(statusCode)
+    .send({ success: false, status: "failed", message: failedMessage });
+
 module.exports = {
   genSecretPassword,
+  failedResponse,
+  successResponse,
+  HTTP_STATUS,
 };
