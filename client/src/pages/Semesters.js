943357119fb79097ef8d9a8104aa9308b08b82d3
Hafiz Muhammad Awais <itxawi@gmail.com>
2022-06-24 12:14:43 +0500

    Re Arrange Semesters Code

--- client/src/pages/Semesters.js
+++ client/src/pages/Semesters.js
@@ -2,53 +2,23 @@ import React, { useState } from "react";
 
 const Semesters = () => {
   //Declaration of Semesters
-  const [semester, setSemetser] = useState("");
-  const [addSemester, setAddSemetser] = useState([]);
+  const [semester, setSemester] = useState("");
 
   //Declaration of Departments Manually
   const [department, setDepartment] = useState("");
-  // useState([
-  //   {
-  //     label: "Computer Science",
-  //     value: "computerScience",
-  //   },
-  //   {
-  //     label: "Food Science & Technology",
-  //     value: "foodScience",
-  //   },
-  //   {
-  //     label: "Bio Technology",
-  //     value: "bioTechnology",
-  //   },
-  //   {
-  //     label: "Fine Arts",
-  //     value: "fineArts",
-  //   },
-  // ]);
-  const [addDepartment, setAddDepartment] = useState([]);
-
-  //Input Event
-  const InputEvent = (event) => {
-    setSemetser(event.target.value);
-    setDepartment(event.target.value);
-  };
 
   //Submit Event
-  const onSubmit = (event) => {
-    console.log("Semester Added");
-    if (!semester) {
+  const onSubmit = () => {
+    console.log("Semester Added = ", semester);
+    console.log("Departmente Added = ", department);
+    if (semester === "" || department === "") {
+      console.log("Type any Name");
     } else {
-      setAddSemetser([...addSemester, semester]); //Add new Semester
-      setAddDepartment([...addDepartment, department]);
+      var modal = { semester, department };
+      console.log(modal);
       setDepartment("");
-      setSemetser("");
+      setSemester("");
     }
-  };
-
-  //Handle Submit
-  const handleSubmit = (event) => {
-    alert("A name was submitted");
-    event.preventDefault();
   };
 
   return (
@@ -59,65 +29,67 @@ const Semesters = () => {
         <h4>Semester</h4>
       </div>
       <div className="container">
-        <form action="" onSubmit={handleSubmit}>
-          <table className="table my-2">
-            <thead className="table-dark">
-              <tr>
-                <th scope="col">Semester</th>
-                <th scope="col">Department</th>
-                <th scope="col"></th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="semester"
-                    placeholder="Enter Semester"
-                    value={semester}
-                    required
-                    onChange={InputEvent}
-                  />
-                </td>
-                <td>
-                  <input
-                    type="text"
-                    className="form-select"
-                    id="department"
-                    placeholder="Enter Department"
-                    value={department}
-                    required
-                    onChange={InputEvent}
-                  />
-                </td>
-                <td>
-                  <button
-                    type="submit"
-                    value="Submit"
-                    className="btn btn-primary mb-4"
-                    onClick={onSubmit}
-                  >
-                    Save
-                  </button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </form>
+        {/* <form action="" onChange={onSubmit}> */}
+        <table className="table my-2">
+          <thead className="table-dark">
+            <tr>
+              <th scope="col">Semester</th>
+              <th scope="col">Department</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="semester"
+                  id="semester"
+                  placeholder="Enter Semester"
+                  value={semester}
+                  required
+                  onChange={(e) => setSemester(e.target.value)}
+                />
+              </td>
+              <td>
+                <input
+                  type="text"
+                  className="form-select"
+                  name="department"
+                  id="department"
+                  placeholder="Enter Department"
+                  value={department}
+                  required
+                  onChange={(e) => setDepartment(e.target.value)}
+                />
+              </td>
+              <td>
+                <button
+                  type="submit"
+                  value="Submit"
+                  className="btn btn-primary mb-4"
+                  onClick={() => onSubmit()}
+                >
+                  Save
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        {/* </form> */}
       </div>
 
       {/* Display Semesters */}
       <div className="container">
         <h3 className="text-left my-2">Semesters List</h3>
-        {addSemester.map((elem) => {
+        {/* {.map((elem) => {
           return (
             <div className="eachItem">
               <h6>{elem}</h6>
             </div>
           );
-        })}
+        })} */}
       </div>
     </>
   );
