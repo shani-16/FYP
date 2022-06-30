6cddf9d29ba7b802be4994162d5b79ba0a603ff6
Camim Khan <camim.xafar500@gmail.com>
2022-07-01 01:50:57 +0500

    login api updated

--- server/routes/auth/login.js
+++ server/routes/auth/login.js
@@ -5,6 +5,11 @@ const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { User } = require("../../models");
+const {
+  failedResponse,
+  HTTP_STATUS,
+  successResponse,
+} = require("../../utils/commonFunctions");
 const JWT_SECRET = process.env.JWT_SECRET;
 const METHOD_NAME_FOR_LOG = "Login User API ERROR";
 router.post(
@@ -14,26 +19,26 @@ router.post(
     body("password", "password cannot be blanked").exists(),
   ],
   async (req, res) => {
-    let success = false;
     const errors = validationResult(req);
+    console.log("errors.array() ", errors.array());
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      failedResponse(res, HTTP_STATUS.BAD_REQUEST, errors.array());
     }
+
+    console.log("req.body - ", req.body);
     const { email, password } = req.body;
     try {
       let user = await User.findOne({ email });
       if (!user) {
-        success = false;
-        return res
-          .status(400)
-          .json({ error: "Please login with correct credentials" });
+        failedResponse(res, HTTP_STATUS.BAD_REQUEST, `${email}  not found`);
       }
       const passwordCompare = await bcrypt.compare(password, user.password);
       if (!passwordCompare) {
-        success = false;
-        return res
-          .status(400)
-          .json({ success, error: "Please login with correct credentials" });
+        failedResponse(
+          res,
+          HTTP_STATUS.BAD_REQUEST,
+          "Please login with correct credentials"
+        );
       }
       const data = {
         user: {
@@ -41,10 +46,14 @@ router.post(
         },
       };
       const authtoken = jwt.sign(data, JWT_SECRET);
-      (success = true), res.json({ success, authtoken });
+      successResponse(res, HTTP_STATUS.OK, "Login Successfull ", authtoken);
     } catch (error) {
       console.error(error.message);
-      res.status(500).send(`${METHOD_NAME_FOR_LOG} ${error.message}`);
+      failedResponse(
+        res,
+        HTTP_STATUS.INTERNAL_SERVER_ERROR,
+        `${METHOD_NAME_FOR_LOG} ${error.message}`
+      );
     }
   }
 );
